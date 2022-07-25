65c51daec7652e38cbb77a72a05eb127e2a889ae
EC2 Default User <ec2-user@ip-172-31-85-222.ec2.internal>
2022-07-25 13:50:20 +0000

    update

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="iiadams/projectmicroservice:firstversion"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+sudo kubectl run projectmicroservice --image=$dockerpath --port=80 --labels app= projectmicroservice
 
 # Step 3:
 # List kubernetes pods
+ sudo kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+sudo kubectl port-forward projectmicroservice 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="iiadams/projectmicroservice:firstversion"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag projectmicroservice $dockerpath 
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
