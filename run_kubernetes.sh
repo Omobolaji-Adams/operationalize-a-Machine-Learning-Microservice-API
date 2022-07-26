811ce2466805cb7c188bfa9ea58fa1e64b1c3025
Ubuntu <ubuntu@ip-172-31-80-65.ec2.internal>
2022-07-26 01:42:55 +0000

    update

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,12 +8,12 @@ dockerpath="iiadams/projectmicroservice:firstversion"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-sudo kubectl run projectmicroservice --image=$dockerpath --port=80 --labels app= projectmicroservice
+kubectl run projectmicroservice --image=iiadams/projectmicroservice:firstversion --port=80 --labels app=projectmicroservice
 
 # Step 3:
 # List kubernetes pods
- sudo kubectl get pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-sudo kubectl port-forward projectmicroservice 8000:80
+kubectl port-forward projectmicroservice 8000:80
